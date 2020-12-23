e714ea568288511f7f2b17d92ac7686e659a188e
yinnping <ciaoing@livemail.tw>
2020-12-23 10:28:06 +0800

    Update example with RxJS

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import commander from "commander";
+import { Observable } from "rxjs";
 
 const program = commander.program;
 program
   .option("-d, --debug", "output extra debugging")
   .option("-s, --small", "small pizza size")
-  .option("-p, --pizza-type <type>", "flavour of pizza");
+  .option("-p, --pizza-type <type>", "flavour of pizza")
+  .option("-r, --rx, rx");
 
 program.parse(process.argv);
 
@@ -12,3 +14,9 @@ if (program.debug) console.log(program.opts());
 console.log("pizza details:");
 if (program.small) console.log("- small pizza size");
 if (program.pizzaType) console.log(`- ${program.pizzaType}`);
+if (program.rx) {
+  new Observable((sub) => {
+    sub.next(1);
+    sub.next(2);
+  }).subscribe(console.log);
+}
